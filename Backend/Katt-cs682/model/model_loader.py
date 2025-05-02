1edacb0bb03c65c6d05a743d85d0ea5b41039d8e
Kattayun Ensafi <61119303+kattens@users.noreply.github.com>
2025-05-02 12:50:58 -0400

    Delete Backend/Katt-cs682/model directory

--- Backend/Katt-cs682/model/model_loader.py
+++
@@ -1,9 +0,0 @@
-from catboost import CatBoostRegressor
-
-def load_model(model_path: str) -> CatBoostRegressor:
-    """
-    Loads a trained CatBoost model from file.
-    """
-    model = CatBoostRegressor()
-    model.load_model(model_path)
-    return model
